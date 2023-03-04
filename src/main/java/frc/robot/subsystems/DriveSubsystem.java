852b35a63b4d539eabac2a2df7eaae49d662739f
ThePrinterFRC1799 <122649904+ThePrinterFRC1799@users.noreply.github.com>
2023-03-03 18:54:26 -0700

    Update DriveSubsystem.java

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,11 +1,15 @@
 package frc.robot.subsystems;
 
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import java.lang.AutoCloseable;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.Constants;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj.PowerDistribution;
 
 public class DriveSubsystem extends SubsystemBase {
 
@@ -20,6 +24,7 @@ public class DriveSubsystem extends SubsystemBase {
       new CANSparkMax(Constants.drive.rf, MotorType.kBrushless));
 
   final DifferentialDrive m_RobotDrive;
+  PowerDistribution PDP = new PowerDistribution();
 
   public DriveSubsystem() {
     leftMotors.setInverted(true);
@@ -29,7 +34,13 @@ public class DriveSubsystem extends SubsystemBase {
     addChild("Drive", m_RobotDrive);
     
   }
-
+public void CheckPull() {
+    for (long canID = 12; canID<19; canID++){
+        if (PDP.getCurrent((int) (canID))>45){
+          c_SparkMax_SetSmartCurrentLimit​(canID, 45, 45, 45);
+        }
+    }
+}
   public void drive(final double ySpeed, final double rotateValue) {
     m_RobotDrive.arcadeDrive(ySpeed, rotateValue);
   }
