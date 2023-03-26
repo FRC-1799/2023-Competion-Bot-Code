297a2262d4889e4785a8164dc202c88c5d28da9c
bakedPotatoLord <918780@dpsk12.net>
2023-03-26 16:54:02 -0600

    edit driveStraight to be position based

--- src/main/java/frc/robot/commands/auto/DriveStraight.java
+++ src/main/java/frc/robot/commands/auto/DriveStraight.java
@@ -1,34 +1,41 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.Constants;
 import frc.robot.subsystems.DriveSubsystem;
 
+public class DriveStraight extends RunCommand {
 
-public class DriveStraight extends WaitCommand {
-  private final DriveSubsystem driveSubsystem;
+  static PIDController controller = new PIDController(
+    Constants.drive.driveForwardsPID.kP,
+    Constants.drive.driveForwardsPID.kI,
+    Constants.drive.driveForwardsPID.kD
+  );
 
-  double speed = 0;
-
-  public DriveStraight(DriveSubsystem driveSubsystem, double time, double speed) {
-    super(time);
-    this.driveSubsystem = driveSubsystem;
-    this.speed = MathUtil.clamp(speed, -1, 1);
-    addRequirements(this.driveSubsystem);
-
+  public DriveStraight(DriveSubsystem driveSubsystem, double distance ) {
+    super(
+      ()->{
+        driveSubsystem.drive(
+          MathUtil.clamp(
+            controller.calculate(driveSubsystem.getAveragePosition()),
+            Constants.drive.driveForwardsPID.outputMin,
+            Constants.drive.driveForwardsPID.outputMax
+          )
+          ,0);
+      }
+    );
+    
+    controller.setSetpoint(distance);
+    controller.setTolerance(
+      Constants.drive.driveForwardsPID.positionTolerance,
+      Constants.drive.driveForwardsPID.velocityTolerance
+    );
+    addRequirements(driveSubsystem);
   }
 
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  public void execute() {
-      super.execute();
-      driveSubsystem.drive(- speed, 0);
-  }
-
-  @Override
-  public void end(boolean interupted){
-    super.end(interupted);
-    driveSubsystem.drive(0,0);
+  public boolean isFinished() {
+    return controller.atSetpoint();
   }
 }
