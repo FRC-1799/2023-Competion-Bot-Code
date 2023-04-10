df9cf3ab5ff0d5f17997ef838729f014af1d3ed2
= <918780@dpsk12.net>
2023-04-09 21:08:01 -0600

    testing out ramsete auto builder

---
+++ src/main/java/frc/robot/commands/auto/AutoTest.java
@@ -0,0 +1,33 @@
+package frc.robot.commands.auto;
+
+import java.util.HashMap;
+
+import com.pathplanner.lib.auto.RamseteAutoBuilder;
+
+import edu.wpi.first.math.controller.RamseteController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.DriveSubsystem;
+
+
+public class AutoTest extends RamseteAutoBuilder{
+  public AutoTest(DriveSubsystem drive) {
+    super(
+      drive::getPose, 
+      drive::resetOdometry,
+      new RamseteController(),
+      drive.kinematics,
+      drive::tankDriveVolts,
+      createMap(
+      ),
+      drive
+      
+    );
+  }
+
+  static HashMap<String,Command> createMap(){
+    HashMap<String,Command> map = new HashMap<>();
+    map.put("halfway",new InstantCommand(()->System.out.println("halfway")));
+    return map;
+  }
+}
